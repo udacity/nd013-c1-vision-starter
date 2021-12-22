d137719f7c3ac412466555085405b0ecfb5f201b
khaled-98 <khaledsoultan63@gmail.com>
2021-12-22 20:06:06 +0000

    Implemented split script

--- create_splits.py
+++ create_splits.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import os
 import random
+from shutil import copy
 
 import numpy as np
 
@@ -17,7 +18,54 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    with open('dark_files.txt') as file:
+        list_of_dark_files = file.readlines()
+        list_of_dark_files = [line.rstrip() for line in list_of_dark_files]
+    
+    with open('rainy_files.txt') as file:
+        list_of_rainy_files = file.readlines()
+        list_of_rainy_files = [line.rstrip() for line in list_of_rainy_files]
+    
+    combined_list_of_special_files = list_of_dark_files + list_of_rainy_files
+
+    list_of_all_files = [os.path.basename(x) for x in glob.glob(source + '/*.tfrecord')]
+    filtered_files = [file for file in list_of_all_files if file not in combined_list_of_special_files]
+
+    training_files = []
+    validation_files = []
+    testing_files = []
+
+    # 75% for training, 15% for validation, 10% testing
+    
+    # filtered files
+    idx_of_75_perct = int(len(filtered_files) * 0.75)
+    idx_of_90_perct = int(len(filtered_files) * 0.9)
+    training_files += filtered_files[0:idx_of_75_perct]
+    validation_files += filtered_files[idx_of_75_perct:idx_of_90_perct]
+    testing_files += filtered_files[idx_of_90_perct:]
+
+    # rainy files
+    idx_of_75_perct = int(len(list_of_rainy_files) * 0.75)
+    idx_of_90_perct = int(len(list_of_rainy_files) * 0.9)
+    training_files += list_of_rainy_files[0:idx_of_75_perct]
+    validation_files += list_of_rainy_files[idx_of_75_perct:idx_of_90_perct]
+    testing_files += list_of_rainy_files[idx_of_90_perct:]
+
+    # dark files
+    idx_of_75_perct = int(len(list_of_dark_files) * 0.75)
+    idx_of_90_perct = int(len(list_of_dark_files) * 0.9)
+    training_files += list_of_dark_files[0:idx_of_75_perct]
+    validation_files += list_of_dark_files[idx_of_75_perct:idx_of_90_perct]
+    testing_files += list_of_dark_files[idx_of_90_perct:]
+
+    for file in training_files:
+        copy(source + '/' + file, destination + '/train/')
+
+    for file in validation_files:
+        copy(source + '/' + file, destination + '/val/')
+
+    for file in testing_files:
+        copy(source + '/' + file, destination + '/test/')
 
 
 if __name__ == "__main__":
