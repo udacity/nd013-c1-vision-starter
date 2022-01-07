57eaa799de52fb3ac414cbc2717d3863b5d7a9ac
qiaunna <shcla13@morgan.edu>
2022-01-07 03:39:58 -0500

    Update create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -5,29 +5,58 @@ import random
 
 import numpy as np
 
+import shutil
+
 from utils import get_module_logger
 
 
-def split(source, destination):
+"""
+NOTE: as described in the project README:
+The training_and_validation folder contains file that have been downsampled: we have selected one every 10 frames from 10 fps videos. The testing folder contains frames from the 10 fps video without downsampling.
+You will split this training_and_validation data into train, and val sets by completing and executing the create_splits.py file.
+So we will not change the "test" folder in the code
+"""
+
+def split(data_dir):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the
+    Create three splits from the processed records. The files should be moved to new folders in the 
     same directory. This folder should be named train, val and test.
-
     args:
-        - source [str]: source data directory, contains the processed tf records
-        - destination [str]: destination data directory, contains 3 sub folders: train / val / test
+        - data_dir [str]: data directory, /home/workspace/data/waymo
     """
+    
+    # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
+    # You should move the files rather than copy because of space limitations in the workspace.
+    
     # TODO: Implement function
+    files = np.array(glob.glob(data_dir + '/training_and_validation/*.tfrecord'))
+    
+    np.random.shuffle(files)
+    # 80-90% is best for training
+    train_ratio = 0.85
+    idx = int(train_ratio * files.shape[0])
 
+    train_files = files[0: idx]
+    val_files = files[idx:]
+    
+    for file in train_files:
+#         print(file)
+        shutil.move(file, data_dir + '/train/')
+    
+    for file in val_files:
+#         print(file)
+        shutil.move(file, data_dir + '/val/')
+    
 
-if __name__ == "__main__":
+    
+# python create_splits.py --data_dir ./data/waymo
+    
+if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser.add_argument('--data_dir', required=True,
+                        help='data directory')
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
     logger.info('Creating splits...')
-    split(args.source, args.destination)
+    split(args.data_dir) 
